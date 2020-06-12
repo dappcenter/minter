fad42ddeaf9388c449a2ffac4c184cc3b0a172a5
Kevin Holder <kevholderx@gmail.com>
2020-06-12 09:24:24 -0400

    IEO url #2

--- src/pages/Root/Root.js
+++ src/pages/Root/Root.js
@@ -84,7 +84,7 @@ const Root = () => {
 		<Suspense fallback={<div></div>}>
 			<RootWrapper>
 				<Announcement>
-					<a href="https://hoo.com/labs/detail/oks">
+					<a href="https://hoo.com/labs/detail/oks2">
 						Announcement: Oikos IEO on Hoo.com from Jun 12 to Jun 14 🚀
 					</a>
 				</Announcement>
