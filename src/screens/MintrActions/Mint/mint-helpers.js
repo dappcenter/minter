ae8654755f4e7dfec28c1ffd01909e9c02a5ff6d
Oikon.cash <admin@oikon.cash>
2020-06-27 16:42:50 -0400

    remove hardcoded price

--- src/screens/MintrActions/Mint/mint-helpers.js
+++ src/screens/MintrActions/Mint/mint-helpers.js
@@ -1,19 +1,19 @@
 import { formatCurrency } from '../../../helpers/formatters';
 import { toNumber, isFinite, isNil } from 'lodash';
 
-export function getStakingAmount({ issuanceRatio, mintAmount, OKSprice = 0.06 }) {
+export function getStakingAmount({ issuanceRatio, mintAmount, OKSprice }) {
 	if (!mintAmount || !issuanceRatio || !OKSprice) return '0';
 	return formatCurrency(mintAmount / issuanceRatio / OKSprice);
 }
 
-export function estimateCRatio({ OKSprice = 0.06, debtBalance, oksBalance, mintAmount }) {
-	console.log(OKSprice, debtBalance, oksBalance, mintAmount);
-	if (isNil(OKSprice) || isNil(debtBalance) || isNil(oksBalance)) {
+export function estimateCRatio({ OKSPrice, debtBalance, oksBalance, mintAmount }) {
+	console.log(OKSPrice, debtBalance, oksBalance, mintAmount);
+	if (isNil(OKSPrice) || isNil(debtBalance) || isNil(oksBalance)) {
 		return 0;
 	}
 
 	const parsedMintAmount = toNumber(mintAmount);
 	const mintAmountNumber = isFinite(parsedMintAmount) ? parsedMintAmount : 0;
-	const oksValue = oksBalance * OKSprice;
+	const oksValue = oksBalance * OKSPrice;
 	return Math.round((oksValue / (debtBalance + mintAmountNumber)) * 100);
 }
